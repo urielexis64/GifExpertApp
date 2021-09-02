2dbecbb9de63a1f5bcd29260ad3a710b712927bf
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-02 02:23:35 -0600

    init GifExpertApp component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,22 +1,19 @@
 import React, {useState} from "react";
 import {AddCategory} from "./components/AddCategory";
+import {GifGrid} from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-	const [categories, setCategories] = useState(["One Punch", "Samurai X", "Dragon Ball"]);
-
-	/* const handleAdd = () => {
-		setCategories([...categories, "HunterXHunter"]);
-	}; */
+	const [categories, setCategories] = useState(["One Punch"]);
 
 	return (
 		<>
 			<h2>GifExpertApp</h2>
-			<AddCategory />
+			<AddCategory setCategories={setCategories} />
 			<hr />
 
 			<ol>
 				{categories.map((category) => (
-					<li key={category}>{category}</li>
+					<GifGrid key={category} category={category} />
 				))}
 			</ol>
 		</>
