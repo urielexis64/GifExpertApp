d73a352d0a7a2c401cf2062c9e36496618480433
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-02 20:07:50 -0600

    get and display data from hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import {useFetchGifs} from "../hooks/useFetchGifs";
 import {GifGridItem} from "./GifGridItem";
 
 export const GifGrid = ({category}) => {
-	const [images, setImages] = useState([]);
-
-	useEffect(() => {
-		getGifs();
-	}, []);
-
-	const getGifs = async (category = "Porn") => {
-		const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=qqXi33Q9UqjGmgK4jNRWYHME0sVGhV9u`;
-		const resp = await fetch(url);
-		const {data} = await resp.json();
-
-		const gifs = data.map((img) => {
-			return {
-				id: img.id,
-				title: img.title,
-				url: img.images?.downsized.url,
-			};
-		});
-		setImages(gifs);
-		// return data;
-	};
+	const {data: images, loading} = useFetchGifs(category);
 
 	return (
 		<>
 			<h3>{category}</h3>
+			{loading && <p>Loading...</p>}
 			<div className='card-grid'>
 				{images.map((img) => (
 					<GifGridItem key={img.id} img={img} />
