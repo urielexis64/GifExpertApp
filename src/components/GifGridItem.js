1bb6a313ef68113a3815e3f6ae1e645866a78e71
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-02 20:08:04 -0600

    open on click gif

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const GifGridItem = ({img}) => {
 	return (
-		<div className='card'>
+		<div className='card' onClick={() => window.open(img.url)}>
 			<img src={img.url} alt={img.title} />
-			<p>{img.title ?? "No description"}</p>
+			<p>{img.title}</p>
 		</div>
 	);
 };
