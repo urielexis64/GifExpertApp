58303d1f35510bcd83a027c7aaf69dfc6c61a37c
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-02 20:36:26 -0600

    add docs

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export const GifGridItem = ({img}) => {
 	return (
-		<div className='card' onClick={() => window.open(img.url)}>
+		<div
+			className='card animate__animated animate__fadeInUp'
+			onClick={() => window.open(img.url)}>
 			<img src={img.url} alt={img.title} />
 			<p>{img.title}</p>
 		</div>
